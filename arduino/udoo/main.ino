adebd76c0260bbbd997d306bbd7adbd68863c985
fitosegrera <fitosegrera@gmail.com>
2015-04-25 18:11:10 -0400

    pins correction

--- arduino/udoo/main.ino
+++ arduino/udoo/main.ino
@@ -1,8 +1,12 @@
 // serialProtocol.ino
 
+int rotMot1 = 62;//Reed switch connected to analog 0 == digital 56
+
 void setup() {
 	Serial.begin(9600);
 	Serial.setTimeout(10);
+	Serial1.begin(9600);
+	pinMode(rotMot1, INPUT);
 }
 
 String data;
@@ -10,5 +14,8 @@ String data;
 void loop() {
 	data = Serial.readStringUntil('%'); //By default the delimiter is '\n' newline
 	Serial.println(data);
+	if(digitalRead(rotMot1)==HIGH){
+		Serial1.println(rotMot1);
+	}
 }
 
